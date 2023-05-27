23bb35183db0c32cc8488849cec5d16bfab8c79a
MrAkashBhardwaj <mrbhardwaj.akash@gmail.com>
2023-05-27 23:28:56 +0530

    first commit

---
+++ src/ClassA.java
@@ -0,0 +1,15 @@
+
+public class ClassA {
+
+	
+	public void method1(){
+		System.out.println("classA - Method 1 - 1st Line");
+	}
+	
+	public void method2(){
+		System.out.println("classA - Method 2 - 1st Line");
+	}
+	
+	
+	
+}
---
+++ src/ClassB.java
@@ -0,0 +1,15 @@
+
+public class ClassB {
+
+	
+	public void method1B(){
+		System.out.println("classB - Method 1 - 1st Line");
+	}
+	
+	public void method2B(){
+		System.out.println("classB - Method 2 - 1st Line");
+	}
+	
+	
+	
+}
