032cd8fb79c50f23a9db4a8bea996d7d6d32acb5
MrAkashBhardwaj <mrbhardwaj.akash@gmail.com>
2023-05-29 15:21:14 +0530

    commit by X in develop

--- src/ClassC.java
+++ src/ClassC.java
@@ -1,5 +1,5 @@
 
-public class ClassB {
+public class ClassC {
 
 	
 	public void method1B(){
@@ -12,11 +12,6 @@ public class ClassB {
 	
 	public void method3B(){
 		System.out.println("classB - Method 3 - 1st Line");
-	}
-	
-	
-	public void method4B(){
-		System.out.println("classB - Method 4 - 1st Line");
 	}
 	
 }
